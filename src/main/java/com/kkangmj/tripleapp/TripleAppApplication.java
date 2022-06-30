07b170a76d680ba7b87bfbfec0dfb8d6899b10ce
minju <bbak0402dd@gmail.com>
2022-06-30 17:38:33 +0900

    feat: User 포인트 이력 관리 테이블 추가

--- src/main/java/com/kkangmj/tripleapp/TripleAppApplication.java
+++ src/main/java/com/kkangmj/tripleapp/TripleAppApplication.java
@@ -2,12 +2,14 @@ package com.kkangmj.tripleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class TripleAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TripleAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TripleAppApplication.class, args);
+  }
 }
--- src/main/java/com/kkangmj/tripleapp/repository/UserRepository.java
+++ src/main/java/com/kkangmj/tripleapp/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.kkangmj.tripleapp.repository;
 
 import com.kkangmj.tripleapp.domain.User;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  User findUserById(UUID id);
+}
