87291b7d4f59173360d8179bedf0d6f03f7a557c
minju <bbak0402dd@gmail.com>
2022-07-04 16:52:50 +0900

    test: 리뷰 삭제에 따른 포인트 증감 테스트 코드 작성

--- src/test/java/com/kkangmj/tripleapp/service/ReviewServiceTest.java
+++ src/test/java/com/kkangmj/tripleapp/service/ReviewServiceTest.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import javax.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,6 +33,13 @@ public class ReviewServiceTest {
   @Autowired private UserPointRepository userPointRepository;
   @Autowired private ReviewRepository reviewRepository;
   @Autowired private PlaceRepository placeRepository;
+
+  @AfterEach
+  void afterEach() {
+    userRepository.deleteAll();
+    reviewRepository.deleteAll();
+    placeRepository.deleteAll();
+  }
 
   @Test
   @DisplayName("content만 존재하는 리뷰 생성 및 포인트 증감 확인 - 보너스 포인트 O")
@@ -197,5 +205,125 @@ public class ReviewServiceTest {
     // then
     assertThat(userPointRepository.findByUserIdQuery(userId).get().getContentPoint()).isEqualTo(1);
     assertThat(reviewRepository.findByReviewUuid(reviewId).get().getContent()).isEqualTo("Hello");
+  }
+
+  @Test
+  @DisplayName("리뷰 삭제 - 특정 장소의 첫 리뷰어인 경우")
+  @Transactional
+  public void deleteReview1() {
+    // given
+    UUID reviewId = UUID.randomUUID();
+    UUID userId = UUID.randomUUID();
+    UUID placeId = UUID.randomUUID();
+
+    User user = User.builder().uuid(userId).build();
+    Place place = Place.builder().uuid(placeId).build();
+
+    userRepository.save(user);
+    placeRepository.save(place);
+
+    EventRequestDto eventRequestDto1 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.ADD,
+            reviewId.toString(),
+            "Hi",
+            new ArrayList<>(List.of("240a0658-dc5f-4878-9381-ebb7b2667775")),
+            userId.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto1);
+
+    // when
+    EventRequestDto eventRequestDto2 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.DELETE,
+            reviewId.toString(),
+            "Hello",
+            new ArrayList<>(),
+            userId.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto2);
+
+    // then
+    assertThat(userPointRepository.findByUserIdQuery(userId).get().getContentPoint()).isEqualTo(0);
+    assertThat(userPointRepository.findByUserIdQuery(userId).get().getBonusPoint()).isEqualTo(0);
+  }
+
+  @Test
+  @DisplayName("리뷰 삭제 - 특정 장소의 첫 리뷰어가 아닌 경우")
+  @Transactional
+  public void deleteReview2() throws InterruptedException {
+    // given
+    UUID reviewId1 = UUID.randomUUID();
+    UUID reviewId2 = UUID.randomUUID();
+    UUID userId1 = UUID.randomUUID();
+    UUID userId2 = UUID.randomUUID();
+    UUID placeId = UUID.randomUUID();
+
+    User user1 = User.builder().uuid(userId1).build();
+    User user2 = User.builder().uuid(userId2).build();
+    Place place = Place.builder().uuid(placeId).build();
+
+    userRepository.save(user1);
+    userRepository.save(user2);
+    placeRepository.save(place);
+
+    EventRequestDto eventRequestDto1 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.ADD,
+            reviewId1.toString(),
+            "Hi",
+            new ArrayList<>(),
+            userId1.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto1);
+
+    EventRequestDto eventRequestDto2 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.ADD,
+            reviewId2.toString(),
+            "Hi",
+            new ArrayList<>(),
+            userId2.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto2);
+
+    Thread.sleep(1000L);
+
+    EventRequestDto eventRequestDto3 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.DELETE,
+            reviewId1.toString(),
+            "Hi",
+            new ArrayList<>(),
+            userId1.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto3);
+
+    // when
+    EventRequestDto eventRequestDto4 =
+        new EventRequestDto(
+            EventType.REVIEW,
+            EventAction.DELETE,
+            reviewId2.toString(),
+            "Hi",
+            new ArrayList<>(),
+            userId2.toString(),
+            placeId.toString());
+
+    reviewService.handleEvent(eventRequestDto4);
+
+    // then
+    assertThat(userPointRepository.findByUserIdQuery(userId2).get().getContentPoint()).isEqualTo(0);
+    assertThat(userPointRepository.findByUserIdQuery(userId2).get().getBonusPoint()).isEqualTo(0);
   }
 }
